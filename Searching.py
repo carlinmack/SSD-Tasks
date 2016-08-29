94f7c801caab1a815303881043f0b657c9a5e556
mackenzieck <mackenzieck@rgc.aberdeen.sch.uk>
2016-08-29 07:39:42 +0100

    Non working Selection sort

--- Searching.py
+++ Searching.py
@@ -4,15 +4,25 @@ array = [5, 3, 10, 24, 26, 1, 7]
 # Insertion
 
 # Selection
+# minVal = [array[0], 0]
+# for k in range(len(array)):
+#     for j in range(1, len(array)):
+#         if minVal[0] > array[j]:
+#             minVal[0] = array[j]
+#             minVal[1] = j
+#     temp = array[k]
+#     array[k] = minVal[0]
+#     minVal[1] = temp
+# print(array)
 
 # Bubble
-swap = True
-while swap:
-    swap = False
-    for i in range(len(array)-1):
-        if array[i] > array[i+1]:
-            swap = True
-            temp = array[i]
-            array[i] = array[i+1]
-            array[i+1] = temp
+# swap = True
+# while swap:
+#     swap = False
+#     for i in range(len(array)-1):
+#         if array[i] > array[i+1]:
+#             swap = True
+#             temp = array[i]
+#             array[i] = array[i+1]
+#             array[i+1] = temp
 print(array)
