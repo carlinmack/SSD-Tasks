50c85321f0d501833dc557c5df0d6b165cca3eba
mackenzieck <mackenzieck@rgc.aberdeen.sch.uk>
2016-08-29 07:56:04 +0100

    Semiworking Insertion sort

--- Searching.py
+++ Searching.py
@@ -3,10 +3,13 @@ array = [5, 3, 10, 24, 26, 1, 7]
 
 # Insertion
 for l in range(1, len(array)):
-    if array[l] < array[l-1]:
+    index = l
+    value = array[l]
+    while index > 0 and value < array[index-1]:
         temp = array[l]
         array[l] = array[l-1]
         array[l-1] = temp
+        index -= 1
 
 # Selection
 # minVal = [array[0], 0]
