256ef79e0f32fb231cc4bfa86de26cf60160a151
mackenzieck <mackenzieck@rgc.aberdeen.sch.uk>
2016-08-29 08:09:54 +0100

    Still not working selection

--- Searching.py
+++ Searching.py
@@ -2,25 +2,24 @@
 array = [5, 3, 10, 24, 26, 1, 7]
 
 # Insertion
-for l in range(1, len(array)):
-    index = l
-    value = array[l]
-    while index > 0 and value < array[index-1]:
-        temp = array[index]
-        array[index] = array[index-1]
-        array[index-1] = temp
-        index -= 1
+# for l in range(1, len(array)):
+#     index = l
+#     value = array[l]
+#     while index > 0 and value < array[index-1]:
+#         temp = array[index]
+#         array[index] = array[index-1]
+#         array[index-1] = temp
+#         index -= 1
 
 # Selection
-# minVal = [array[0], 0]
-# for k in range(len(array)):
-#     for j in range(1, len(array)):
-#         if minVal[0] > array[j]:
-#             minVal[0] = array[j]
-#             minVal[1] = j
-#     temp = array[k]
-#     array[k] = minVal[0]
-#     minVal[1] = temp
+for k in range(len(array)):
+    minVal = k
+    for j in range(1, len(array)):
+        if array[minVal] > array[j]:
+            minVal = j
+    temp = array[k]
+    array[k] = array[minVal]
+    array[minVal] = temp
 
 # Bubble
 # swap = True
