59f2d24b22212adb36bc73959911f1d117bce598
mackenzieck <mackenzieck@rgc.aberdeen.sch.uk>
2016-09-12 10:13:22 +0100

    Completed program, no internal commentry

---
+++ Grand Prix.py
@@ -0,0 +1,55 @@
+# Carlin MacKenzie, 12/09
+
+driverDictionary = []
+def inputDictionaries(driverDictionary):
+    with open("racing.csv") as readFile:
+        line = readFile.readline().rstrip("\n")
+        while line:
+            items = line.split(",")
+
+            Driver = items[0]
+            Team = items[1]
+            Time = items[2]
+            Points = items[3]
+
+            driverDictionary.append({"Driver": Driver, "Team": Team, "Time": Time, "Points": Points})
+            line = readFile.readline().rstrip("\n")
+    return driverDictionary
+
+def userInput():
+    for i in range(len(driverDictionary)):
+        temp = input("Race time for " + str(driverDictionary[i]["Driver"]) + " is... ")
+        driverDictionary[i]["Time"] = temp
+
+def sortDrivers():
+    swap = True
+    while swap:  # runs until it is sorted
+        swap = False  # resets flag
+        for i in range(len(driverDictionary) - 1):  # traverses whole driverDictionary array
+            if driverDictionary[i]["Time"] > driverDictionary[i + 1]["Time"]:  # if values are unsorted
+                swap = True  # triggers flag
+                # swaps the unsorted adjacent values
+                temp = driverDictionary[i]
+                driverDictionary[i] = driverDictionary[i + 1]
+                driverDictionary[i + 1] = temp
+
+def assignPoints():
+    driverDictionary[0]["Points"] = 25
+    driverDictionary[1]["Points"] = 18
+    driverDictionary[2]["Points"] = 15
+
+def displayValues():
+    print("Driver".ljust(22) + "Team".ljust(15) +"Time".ljust(10) + "Points".ljust(10))
+    for j in range(len(driverDictionary)):
+        temp = driverDictionary[j]["Driver"].ljust(22)
+        temp += driverDictionary[j]["Team"].ljust(15)
+        temp += str(driverDictionary[j]["Time"]).ljust(10)
+        temp += str(driverDictionary[j]["Points"]).ljust(10)
+        print(temp)
+    
+# -------Main-------
+inputDictionaries(driverDictionary)
+userInput()
+sortDrivers()
+assignPoints()
+displayValues()
